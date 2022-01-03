aad40a753baa7c9b234ea815824f937a332c3fb9
Sudhansu Shrestha <sudhansu.shrestha@cgifederal.com>
2022-01-03 13:09:15 -0500

    Sample

---
+++ pipeline.groovy
@@ -0,0 +1,20 @@
+@Library('pipeline_lib') _
+
+pipeline{
+    agent any
+    parameters {
+        string(name: 'release_number', defaultValue: '0.0.0.1', description: 'Release Number for the Application')
+    }
+    stages{
+        stage("Initialize Pipeline"){
+            steps{
+                sayHello "Dave"
+            }
+        }
+        stage("Build"){
+            steps{
+                build_mvn([tool: "mvn", args: "-Drelease_number=${params.release_number}"])
+            }
+        }
+    }
+}
