725cd7ab4063f4dd1de6d423afc972f95e822ca4
Sudhansu Shrestha <sudhansu.shrestha@cgifederal.com>
2022-01-04 14:34:12 -0500

    try scmurl

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,15 +6,21 @@ pipeline{
         string(name: 'release_number', defaultValue: '0.0.0.1', description: 'Release Number for the Application')
     }
     stages{
-        // stage("Initialize Pipeline"){
-        //     steps{
-        //         sayHello "Dave"
-        //     }
-        // }
+        stage("Initialize Pipeline"){
+            steps{
+                sayHello "Dave"
+            }
+        }
         stage("Build"){
             steps{
                 build_mvn([tool: "mvn", args: "-Drelease_number=${params.release_number}"])
             }
+        }
+    }
+    post {
+        // Clean after build
+        always {
+            cleanWs()
         }
     }
 }
